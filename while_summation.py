83feaa9bd2d0eb7bc4bd26245c7a934d9be553e5
CoolGuyTrace <christra011@alpinesd.org>
2024-10-22 09:37:07 -0600

    Finish while summation

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,15 @@
 #while summation code following directions from Readme
+userNumber = int(input("Enter a number: "))
+num1 = 0
+num2 = 0
+
+while num1 != userNumber:
+    #Add all the numbers before together
+    num1+=1
+    num2+=num1
+
+#Print the final sum
+print(num2)
+
+
+
