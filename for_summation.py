caad37c4e09b090733a579068cda8924d02a0f0a
CoolGuyTrace <christra011@alpinesd.org>
2024-10-24 09:31:02 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -3,7 +3,7 @@ num1 = 0
 
 #Get the user's number then add 1 so the entered number will also be added
 for i in range(int(input("Enter a number: "))+1):
-    num1+=2
+    num1+=i
 
 #Print the final sum.
 print(num1)
